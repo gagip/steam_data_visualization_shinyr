336d4ee0431955e00d5f41e05ccb709ece1981b6
gagip <y02645@gmail.com>
2019-12-13 08:30:15 +0900

    f

--- app.R
+++ app.R
@@ -9,6 +9,8 @@ library(shiny)
 
 # radarchart
 library(fmsb)
+#remotes::install_github("gusef/d3Toolbox")
+require(d3Toolbox)
 
 
 load("data/steam.tag.table.RData")
@@ -28,66 +30,73 @@ ui <- fluidPage(
                # 통계요약 탭
                #-----------------------------------------
                tabPanel("통계요약",
-                        #-----------------------------------------
-                        # 통계요약 탭::input
-                        #-----------------------------------------
-                        fluidRow(
-                            column(3,
-                                   # 장르
-                                   selectInput(inputId = "genres",
-                                               label = "장르 선택",
-                                               choices = c("Action", "Indie", "RPG", 
-                                                           "Strategy", "Simulation", "Casual",
-                                                           "Sports", "Racing"),
-                                               multiple = T,
-                                               selected = "Action")
-                            ),
-                            column(3,
-                                   # 날짜
-                                   dateRangeInput(inputId = "DateRange",
-                                               label = "날짜",
-                                               start = "2006-01-01",
-                                               end = "2019-04-22")
-                            ),
-                            column(3,
-                                   # 랭킹 기준
-                                   selectInput(inputId = "rank_criterion",
-                                               label = "랭킹 기준",
-                                               choices = c("인기순" = "total_vote",
-                                                           "긍정 수" = "positive_ratings",
-                                                           "부정 수" = "negative_ratings",
-                                                           "예상 매출액" = "pred_sales"))
-                            ),
-                            column(3, 
-                                   # 상위
-                                   sliderInput(inputId = "ranking",
-                                               label = "상위 몇 퍼?",
-                                               min = 0,
-                                               max = 100,
-                                               value = c(0,10))
-                            )
-                        ),
                         
-                        fluidRow(
-                            column(3,
-                                   selectInput(inputId = "plot_type",
-                                               label = "보시고 싶은 정보",
-                                               choices = c("년도별 출시 빈도" = "release_by_year",
-                                                           "좋아요/싫어요" = "positive_negative",
-                                                           "장르별 예상매출량" = "sales_by_year"
-                                                           )
-                                               )
-                                   )
-                        ),
-                        #-----------------------------------------
-                        # 통계요약 탭::output
-                        #-----------------------------------------
-                        hr(),
-                        plotOutput(outputId = "GamePlot", width = "100%", height = 600, 
-                                   hover = hoverOpts("plot_hover")),
-                        uiOutput("hover_info"),
-                        plotOutput(outputId = "free_plot",width = "100%", height = 600),
-                        DT::dataTableOutput(outputId = "GameData")
+                        sidebarLayout(
+                            #-----------------------------------------
+                            # 통계요약 탭::input
+                            #-----------------------------------------
+                            sidebarPanel(
+                                conditionalPanel(
+                                    'input.mainplot1 === "그래프"',
+                                    # 장르
+                                    selectInput(inputId = "genres",
+                                                label = "장르 선택",
+                                                choices = c("Action", "Indie", "RPG", 
+                                                            "Strategy", "Simulation", "Casual",
+                                                            "Sports", "Racing"),
+                                                multiple = T,
+                                                selected = c("Action", "Indie", "RPG", "Simulation")),
+                                    # 날짜
+                                    dateRangeInput(inputId = "DateRange",
+                                                   label = "날짜",
+                                                   start = "2006-01-01",
+                                                   end = "2019-04-22"),
+                                    # 랭킹 기준
+                                    selectInput(inputId = "rank_criterion",
+                                                label = "랭킹 기준",
+                                                choices = c("인기순" = "total_vote",
+                                                            "긍정 수" = "positive_ratings",
+                                                            "부정 수" = "negative_ratings",
+                                                            "예상 매출액" = "pred_sales")),
+                                    # 상위
+                                    sliderInput(inputId = "ranking",
+                                                label = "상위 몇 퍼?",
+                                                min = 0,
+                                                max = 100,
+                                                value = c(0,100)),
+                                    
+                                    selectInput(inputId = "plot_type",
+                                                label = "보시고 싶은 정보",
+                                                choices = c("년도별 출시 빈도" = "release_by_year",
+                                                            "좋아요/싫어요" = "positive_negative",
+                                                            "장르별 예상매출량" = "sales_by_year"
+                                                ))
+                                ),
+                                conditionalPanel(
+                                    'input.mainplot1 === "GamePlot222"'
+                                ),
+                                
+                                
+                                width = 3
+                            ),
+                            #-----------------------------------------
+                            # 통계요약 탭::output
+                            #-----------------------------------------
+                            mainPanel(
+                                tabsetPanel(
+                                    id = "mainplot1",
+                                    tabPanel(
+                                        "그래프" , plotOutput(outputId = "GamePlot", width = "100%", height = 600)
+                                        # d3BarplotOutput(outputId = "GamePlot", width = "100%", height = 600)
+                                    ),
+                                    tabPanel(
+                                        "표", DT::dataTableOutput(outputId = "GameData")
+                                    )
+                                ),
+                                
+                                width = 9
+                            )
+                        )
                ),
                
                #-----------------------------------------
@@ -156,7 +165,9 @@ server <- function(input, output) {
     # 통계요약 테이블
     #-----------------------------------------
     output$GameData = DT::renderDataTable({
-        sample_data()
+        sample_data() %>% select(name, release_date, developer, genres, steamspy_tags,
+                                 positive_ratings, negative_ratings, average_playtime_h,
+                                 owners, price)
     })
     
     
@@ -165,14 +176,31 @@ server <- function(input, output) {
     #-----------------------------------------
     # 통계요약 테이블 그래프
     #-----------------------------------------
+    # output$GamePlot = renderd3Barplot({
+    #     require(d3Toolbox)
+    #     data <- 1:15
+    #     names(data) <- c(LETTERS[1:15])
+    #     
+    #     d3Barplot(data,
+    #               col=c('steelblue'),
+    #               xlab='Letters',
+    #               ylab='Frequencies',
+    #               title='New Barplot',
+    #               subtitle='with subtitle')
+    # })
     output$GamePlot <- renderPlot({
         # 테마 및 plot 세팅 통일 (글씨 크기 등)
+        theme_setting = theme(axis.text.x = element_text(face = "bold", size = 15),
+                              axis.text = element_text(size = 15, face = "bold"),
+                              axis.title = element_text(size = 20, face = "bold"),
+                              legend.text = element_text(size = 13, face = "bold"),
+                              legend.title = element_text(size = 15, face = "bold"))
         
         # 출시 빈도
         if (input$plot_type == "release_by_year"){
-            sample_data() %>% 
+            sample_data() %>%
                 # 집계
-                group_by(year_quarter) %>%
+                group_by(year) %>%
                 summarise(Indie = sum(Indie),
                           Strategy = sum(Strategy),
                           RPG = sum(RPG),
@@ -180,35 +208,32 @@ server <- function(input, output) {
                           Casual = sum(Casual),
                           Sports = sum(Sports),
                           Racing = sum(Racing),
-                          Action = sum(Action)) %>% 
+                          Action = sum(Action)) %>%
                 # 데이터 변환
-                gather(key = "genre", value = "value", -1) %>% 
+                gather(key = "genre", value = "value", -1) %>%
                 # 장르 필터
                 filter(genre %in% input$genres) %>%
                 # 시각화
-                ggplot( aes( x = year_quarter,
+                ggplot( aes( x = factor(year),
                              y = value,
                              group = genre,
-                             col = genre)) + 
-                geom_smooth(method = "auto", se=F) + 
-                theme(axis.text.x = element_text(angle = -90, face = "bold", size = 15),
-                                                           axis.text = element_text(size = 15, face = "bold"),
-                                                           axis.title = element_text(size = 20, face = "bold"),
-                                                           legend.text = element_text(size = 13, face = "bold"),
-                                                           legend.title = element_text(size = 15, face = "bold"))
-
-                
-                
-                
+                             fill = genre)) +
+                geom_bar(stat = "identity") +
+                geom_text(aes(label=ifelse(value>20, value,"")),
+                          position = position_stack(vjust=0.5)) +
+                # 커스텀
+                labs(x="년도", y="출시 수(개)") +
+                scale_y_continuous(labels = comma) +
+                theme_setting
         }
         # 긍정/부정 비율
         else if (input$plot_type == "positive_negative"){
             pn_plot = sample_data() %>%
                 # 데이터 전처리
-                ggplot( aes( x = like_rate, 
+                ggplot( aes( x = like_rate,
                              y = total_like_unlike)) +
                 geom_point() +
-                geom_vline(xintercept = 0.5) + 
+                geom_vline(xintercept = 0.5) +
                 theme(axis.text.x = element_text(face = "bold", size = 15),
                       axis.text = element_text(size = 15, face = "bold"),
                       axis.title = element_text(size = 20, face = "bold"),
@@ -240,7 +265,7 @@ server <- function(input, output) {
                        Casual_sales = Casual * pred_sales,
                        Sports_sales = Sports * pred_sales,
                        Racing_sales = Racing * pred_sales) %>%
-                group_by(year_quarter) %>%
+                group_by(year) %>%
                 summarise(Action = sum(Action_sales),
                           Indie = sum(Indie_sales),
                           Strategy = sum(Strategy_sales),
@@ -249,16 +274,23 @@ server <- function(input, output) {
                           Casual = sum(Casual_sales),
                           Sports = sum(Sports_sales),
                           Racing = sum(Racing_sales)) %>%
-                gather(key = "genre", 
+                gather(key = "genre",
                        value = "value",-1) %>%
                 filter(genre %in% input$genres) %>%
-                ggplot(aes(x = year_quarter,
+                # 단위 환산
+                mutate(value = round(value/1000000,2)) %>%
+                # 시각화
+                ggplot(aes(x = factor(year),
                            y = value,
                            group = genre,
-                           col = genre)) +
-                geom_smooth(se = F) +
-                scale_y_continuous(label = comma) + 
-                theme(axis.text.x = element_text(angle = -90, face = "bold", size = 15),
+                           fill = genre)) +
+                geom_bar(stat="identity") +
+                geom_text(aes(label=ifelse(value>200, paste0("$",round(value,0)),"")),
+                          position = position_stack(vjust=0.5)) +
+                # 커스텀
+                labs(x="년도", y="출시 수(백만 달러)") +
+                scale_y_continuous(label = dollar) +
+                theme(axis.text.x = element_text(face = "bold", size = 15),
                       axis.text = element_text(size = 15, face = "bold"),
                       axis.title = element_text(size = 20, face = "bold"),
                       legend.text = element_text(size = 13, face = "bold"),
@@ -279,16 +311,16 @@ server <- function(input, output) {
             # 인기 순 # 내가 선택한 태그들이 골고루 들어있어야 함
             temp = temp %>% arrange(-mean,var,-total)
         }
-            
+        
     })
-
+    
     output$recommend_game_text = renderPrint({
         top10 = tag_data()$appid %>% head(3)
         top10_name = data.focus %>% filter(appid %in% top10) %>% .$name %>% as.character()
         top10_name
     })
     
-
+    
     output$radar = renderPlot({
         dd = tag_data() %>% select(-mean, -var, -total) %>% head(3)
         dd = as.data.frame(dd, row.names = dd$appid) # 행 이름으로 만들자
@@ -306,13 +338,13 @@ server <- function(input, output) {
                    cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8,
                    vlcex=0.8)
     })
-        
+    
     # 디버깅용
     output$recommend_game_data = DT::renderDataTable({
         tag_data()
     })
     
-
+    
 }
 
 # Run the application 
